40f47e8f985005db3707022613539b502f984be8
Justin Dressel <dressel@chapman.edu>
2018-09-10 23:41:09 -0700

    INSTRUCTOR COMMENTS

--- helloworld.sh
+++ helloworld.sh
@@ -5,3 +5,9 @@
 echo 'Hello World!'
 echo $?
 0
+
+# INSTRUCTOR COMMENT:
+# should be
+# exit 0
+# (0 is success, and is passed directly to exit)
+# (The variable $? contains the exit code of the last executed program)
