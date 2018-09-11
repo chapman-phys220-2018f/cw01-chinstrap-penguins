530e255848b906e49d52f8ce1adfbab238d887f8
Justin Dressel <dressel@chapman.edu>
2018-09-10 23:41:55 -0700

    INSTRUCTOR COMMENT

--- createfiles.sh
+++ createfiles.sh
@@ -6,6 +6,9 @@ cd tmpfiles
 
 for i in $(seq 10); do echo > file${i}.tmp; done
 
+# INSTRUCTOR COMMENT:
+# You can also use the "touch" command to create an empty file.
+
 for i in $(seq 10); do echo "Temp File "${i} >> file${i}.tmp; done
 
 cd -
